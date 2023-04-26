5b5c79461b293c8d5902dac2bbe2872b8c638aa6
t.akasaki <t.akasaki.csharp@gmail.com>
2023-04-27 00:21:51 +0900

    refactor: :recycle: test.each以外のeach関数を評価できるようにリファクタ

--- src/util.ts
+++ src/util.ts
@@ -2,16 +2,22 @@ import { Rule, SourceCode } from 'eslint';
 
 type Node = Parameters<Exclude<Rule.NodeListener['TaggedTemplateExpression'], undefined>>[0];
 
+const matchList = [
+  'test.each'
+] as const;
+type MatchString = typeof matchList[number];
+
 type Result = {
   range: Exclude<Node['range'], undefined>
   loc: Exclude<Node['loc'], undefined | null>
   source: string
   header: string[]
   splitCharCount: number
+  matchString: MatchString
 }
 
 /**
- * test.eachの変数を取得する
+ * jest eachの変数を取得する
  * @param node 
  */
 export function getTestEachVariables(node: Node, sourceCode: SourceCode): Result | undefined {
@@ -24,9 +30,12 @@ export function getTestEachVariables(node: Node, sourceCode: SourceCode): Result
 
   // test.eachでなければ終了
   const source = sourceCode.getText(node);
-  if(!source.match(/^test\.each.*/)) {
+
+  const match = (matchList.map(m => source.match(new RegExp(`^${m.replace('.', '\.')}.*`))).filter(f => f !== null) as RegExpMatchArray[]);
+  if(match.length === 0 || match[0].length === 0) {
     return;
   }
+  const matchString = match[0][0].split('`')[0] as MatchString;
 
   // テンプレート文字列を取得
   const [headerArray, ...templateString] = node.quasi.quasis
@@ -39,6 +48,7 @@ export function getTestEachVariables(node: Node, sourceCode: SourceCode): Result
     loc, 
     source, 
     header,
+    matchString,
     splitCharCount: templateString.length
   };
 }
